fee54cfedad4f90d2d89a9106eb85c22b93d8342
Shiara <shiara@bpoc.co.jp>
2023-09-20 13:25:34 +0800

    improve the handleLoginRequest function

--- html/login/controllers/authentication.php
+++ html/login/controllers/authentication.php
@@ -17,34 +17,33 @@ class AuthenticationController {
         }
     }
 
+    
     public function handleLoginRequest() {
-        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-            try {
-                if (!isset($_POST['username']) || !isset($_POST['password'])) {
-                    throw new Exception("Username and password are required.");
-                }
-
-                $username = $_POST['username'];
-                $password = $_POST['password'];
-
-                if (empty($username)) {
-                    throw new Exception("Username is required");
-                } 
-                
-                if (empty($password)) {
-                    throw new Exception("Password is required");
-                }
-
-                $this->authenticate($username, $password);
-            } catch (Exception $e) {
-                header("Location: ../views/login.php?error=" . urlencode($e->getMessage()));
-                exit();
-            }
-        } else {
+        if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
             header("Location: ../views/login.php");
             exit();
         }
-    }
+
+        if (!isset($_POST['username']) || !isset($_POST['password'])) {
+            header("Location: ../views/login.php?error=" . urlencode("Username and Password are required."));
+        exit();
+        }
+
+        $username = $_POST['username'];
+        $password = $_POST['password'];
+
+        if (empty($username)) {
+            header("Location: ../views/login.php?error=" . urlencode("Username is required"));
+            exit();
+        } 
+
+        if (empty($password)) {
+            header("Location: ../views/login.php?error=" . urlencode("Password is required"));
+            exit();
+        }
+
+        $this->authenticate($username, $password);
+    }   
 }
 
 $authController = new AuthenticationController();
