154e60f5854df7061f8cc873f675d1164f0e5109
Shiara <shiara@bpoc.co.jp>
2023-09-20 10:27:00 +0800

    modified how error message is shown

--- html/login/views/login.php
+++ html/login/views/login.php
@@ -8,7 +8,7 @@
         <form action="../controllers/authentication.php" method="post">
             <h2>LOGIN</h2>
             <?php if (isset($_GET['error'])) { ?>
-                <p class="error"><?php echo $_GET['error']; ?></p>
+                <p class="error"><?= $_GET['error']; ?></p>
             <?php } ?>
             <label>Username</label>
             <input type="text" name="username" placeholder="Username"><br>
